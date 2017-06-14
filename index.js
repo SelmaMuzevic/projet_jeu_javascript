e65b32e843bee87e290ba7f737f44d5d5403ade6
selma <selma@gmail.com>
2017-06-14 15:19:42 +0200

    changement des figurines et mettre a jour

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
  "use strict";
 
- let pocahontas = {
-     amour: 200,
-     bonheur: 100,
+ let monstre = {
+     energie: 200,
+     vies: 100,
      coeur: 50,
      sante: 300
  };
 
- let john = {
-     amour: 200,
-     bonheur: 100,
+ let tombrider = {
+     energie: 200,
+     vies: 100,
      coeur: 50,
      sante: 300
- }
+ };
 
 
  document.body.querySelector("#attack").addEventListener("click", function(e) {
@@ -21,14 +21,30 @@
      gameOver();
  });
 
+ document.body.querySelector("#crier").addEventListener("click", function(e) {
 
- document.body.querySelector("#embrouille").addEventListener("click", function(e) {
-     embrouille();
      affichage();
      gameOver();
  });
- document.body.querySelector("#bisous").addEventListener("click", function(e) {
-     bisous();
+ document.body.querySelector("#frapper").addEventListener("click", function(e) {
+
+     affichage();
+     gameOver();
+ });
+
+ document.body.querySelector("#attack2").addEventListener("click", function(e) {
+     attack();
+     affichage();
+     gameOver();
+ });
+
+ document.body.querySelector("#arrow").addEventListener("click", function(e) {
+
+     affichage();
+     gameOver();
+ });
+ document.body.querySelector("#gun").addEventListener("click", function(e) {
+
      affichage();
      gameOver();
  });
@@ -36,62 +52,41 @@
  affichage()
 
  function gameOver() {
-     if (john.sante <= 0) {
+     if (tombrider.sante <= 0) {
          alert("GAME OVER !");
      }
-     if (pocahontas.sante <= 0) {
+     if (monstre.sante <= 0) {
          alert("GAME OVER !");
      }
  }
-
-
 
 
  function affichage() {
-     document.querySelector(".PocaStats .amour").textContent = pocahontas.amour;
-     document.querySelector(".PocaStats .bonheur").textContent = pocahontas.bonheur;
-     document.querySelector(".PocaStats .coeur").textContent = pocahontas.coeur;
-     document.querySelector(".PocaStats .sante").textContent = pocahontas.sante;
+     document.querySelector(".MonstreStats .energie").textContent = monstre.energie;
+     document.querySelector(".MonstreStats .vies").textContent = monstre.vies;
+     document.querySelector(".MonstreStats .coeur").textContent = monstre.coeur;
+     document.querySelector(".MonstreStats .sante").textContent = monstre.sante;
 
-     document.querySelector(".JohnStats .amour").textContent = john.amour;
-     document.querySelector(".JohnStats .bonheur").textContent = john.bonheur;
-     document.querySelector(".JohnStats .coeur").textContent = john.coeur;
-     document.querySelector(".JohnStats .sante").textContent = john.sante;
+     document.querySelector(".TombriderStats .energie").textContent = tombrider.energie;
+     document.querySelector(".TombriderStats .vies").textContent = tombrider.vies;
+     document.querySelector(".TombriderStats .coeur").textContent = tombrider.coeur;
+     document.querySelector(".TombriderStats .sante").textContent = tombrider.sante;
  }
 
  function attack() {
-     pocahontas.amour -= 20;
-     pocahontas.bonheur -= 50;
-     pocahontas.coeur -= 25;
-     pocahontas.sante -= 50;
 
-     john.amour -= 20;
-     john.bonheur -= 50;
-     john.coeur -= 25;
-     john.sante -= 50;
+     tombrider.energie -= 20;
+     tombrider.vies -= 50;
+     tombrider.coeur -= 25;
+     tombrider.sante -= 50;
  }
 
- function embrouille() {
-     pocahontas.amour -= 20;
-     pocahontas.bonheur -= 50;
-     pocahontas.coeur -= 25;
-     pocahontas.sante -= 30;
 
+ function attack2() {
 
-     john.amour -= 20;
-     john.bonheur -= 50;
-     john.coeur -= 25;
-     john.sante -= 50;
- }
+     monstre.energie -= 20;
+     monstre.vies -= 50;
+     monstre.coeur -= 25;
+     monstre.sante -= 50;
 
- function bisous() {
-     pocahontas.amour += 20;
-     pocahontas.bonheur += 50;
-     pocahontas.coeur += 25;
-     pocahontas.sante = 100;
-
-     john.amour += 20;
-     john.bonheur += 50;
-     john.coeur += 25;
-     john.sante = 100;
  }
